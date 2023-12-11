4bf3fe74082ef26802e9e3f80f020edc8ab8aa7b
Albin Rodriguez <ealbinu@gmail.com>
2023-12-11 10:57:59 -0600

    update

--- public/config.app.js
+++ public/config.app.js
@@ -32,8 +32,8 @@ var _a = {
         terms: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.',
         privacy: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.'
     },
-    secure3d: true,
+    secure3d: false,
     expiredlink: false,
-    ispaid: true,
+    ispaid: false,
 }
 
